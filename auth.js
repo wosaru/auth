d9fe2494dc4e19e48e083ef10e3c1c0d444a94b3
wosaru <next20180208.2@gmail.com>
2026-05-11 02:22:05 +0900

    Update auth.js

--- auth.js
+++ auth.js
@@ -6,11 +6,11 @@
 
 // ▼ パスワード（複数OK）
 const PASSWORDS = [
-  "1234"
+  "1670"
 ];
 
 // ▼ 変更時に数字を変える（全員強制ログアウト）
-const AUTH_VERSION = "20260223";
+const AUTH_VERSION = "20260510";
 
 // ▼ Cookie名（変更不要）
 const COOKIE_NAME = "shared_tool_auth";
